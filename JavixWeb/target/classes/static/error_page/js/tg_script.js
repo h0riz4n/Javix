911fca74ffbdb4ec69449d8c818e8a42c13bf237
Rustam <96946731+h0riz4n@users.noreply.github.com>
2023-03-31 19:54:44 +0300

    Delete JavixWeb/target/classes directory

--- JavixWeb/target/classes/static/error_page/js/tg_script.js
+++
@@ -1,8 +0,0 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-
-document.addEventListener("click", (event) => {
-  if (event.target.className == "header__cancel-icon") {
-    tg.close();
-  }
-});
